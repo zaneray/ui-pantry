7c47d8d73254464ab8753e5fdc476be3b1d894c6
Colin Baxter <colin.baxter@zaneray.com>
2019-07-16 14:53:56 -0600

    Initial component setup

---
+++ packages/vue/components/BaseButton/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button>{{label}}</button>
+</template>
+
+<script>
+  export default {
+    name: "BaseButton",
+    props: {
+      label: {
+        type: String,
+        default: 'Base Button'
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<docs>
+    ### Examples
+
+    #### Basic Button
+    ```jsx
+    <BaseButton :label="'Button Label'"></BaseButton>
+    ```
+</docs>
