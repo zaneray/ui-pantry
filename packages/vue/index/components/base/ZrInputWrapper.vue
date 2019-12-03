50bc0d171b11d9836be1d7c218c64b58ccea2c9d
Mason Mullendore <mason.mullendore@zaneray.com>
2019-12-03 09:59:30 -0700

    layout and order for required message

--- packages/vue/index/components/base/ZrInputWrapper.vue
+++ packages/vue/index/components/base/ZrInputWrapper.vue
@@ -27,6 +27,29 @@
             width: 100%;
         }
 
+        &.required {
+            display: flex;
+            flex-wrap: wrap;
+            align-items: baseline;
+            justify-content: space-between;
+
+            .required-label {
+                order: 2;
+            }
+
+            label {
+                order: 1;
+            }
+
+            input {
+                order: 3;
+            }
+
+            .invalid-message {
+                order: 4;
+            }
+        }
+
         .invalid-message {
             color: $color-warning;
         }
